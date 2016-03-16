80cf0257bd05705b791445524997811e99483244
cnzsb <cnzsb@foxmail.com>
2016-03-16 10:46:00 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -12,42 +12,12 @@ var gulp = require('gulp'),
     imagemin = require('gulp-imagemin'),    
     browserSync = require('browser-sync');
 
-// 暂时没有拼接，仅copy到新目录
-// gulp.task('html', function () {
-// 	return gulp.src('public/**/*.html', {base: 'src'})
-// 		.pipe(gulp.dest('dist/'))
-// 		.pipe(notify({message: 'HTML done!'}));
-// });
-
 gulp.task('sass', function () {
-	return sass('public/scss/*.scss', {base: 'src'}, {style: 'expanded'})
+	return sass('2016_spring/scss/*.scss', {base: 'src'}, {style: 'expanded'})
 		.pipe(autoprefixer('last 2 version', 'ie >= 7'))
-		.pipe(gulp.dest('public/css'))
+		.pipe(gulp.dest('2016_spring/css'))
 		.pipe(notify({message: 'Sass done!'}));
 });
-
-// gulp.task('css', function () {
-// 	return gulp.src('public/**/*.css', {base: 'src'})
-// 		.pipe(autoprefixer('last 2 version', 'ie >= 7'))
-// 		.pipe(gulp.dest('dist/'))
-// 		.pipe(notify({message: 'CSS done!'}));
-// });
-
-// 暂时关闭jshint,IDE中默认使用jshint
-// gulp.task('js', function () {
-// 	return gulp.src('public/**/*.js', {base: 'src'})
-// 		// .pipe(jshint())
-// 		// .pipe(jshint.reporter('default'))
-// 		.pipe(gulp.dest('dist/'))
-// 		.pipe(notify({message: 'JS done!'}));
-// });
-
-// gulp.task('img', function () {
-// 	return gulp.src('public/**/images/*', {base: 'src'})
-// 		.pipe(imagemin({optimizationLevel: 3, progressive: true, interlaced: true}))
-// 		.pipe(gulp.dest('dist/'))
-// 		.pipe(notify({message: 'Images done!'}))
-// });
 
 // 默认生成所有文件
 // gulp.task('default', ['html', 'sass', 'css', 'js', 'img']);
@@ -68,18 +38,18 @@ gulp.task('refresh', ['sass'], function () {
 		}
 	});
 
-	// gulp.watch('public/**/*.html', ['html']);
-	gulp.watch('public/**/*.scss', ['sass']);
-	// gulp.watch('public/**/*.css', ['css']);
-	// gulp.watch('public/**/*.js', ['js']);
-	// gulp.watch('public/**/images/*', ['img']);
+	// gulp.watch('2016_spring/**/*.html', ['html']);
+	gulp.watch('2016_spring/**/*.scss', ['sass']);
+	// gulp.watch('2016_spring/**/*.css', ['css']);
+	// gulp.watch('2016_spring/**/*.js', ['js']);
+	// gulp.watch('2016_spring/**/images/*', ['img']);
 });
 
 // 监听所有文件并实时生成
 gulp.task('watch', ['sass'], function () {
-	// gulp.watch('public/**/*.html', ['html']);
-	gulp.watch('public/**/*.scss', ['sass']);
-	// gulp.watch('public/**/*.css', ['css']);
-	// gulp.watch('public/**/*.js', ['js']);
-	// gulp.watch('public/**/images/*', ['img']);
+	// gulp.watch('2016_spring/**/*.html', ['html']);
+	gulp.watch('2016_spring/**/*.scss', ['sass']);
+	// gulp.watch('2016_spring/**/*.css', ['css']);
+	// gulp.watch('2016_spring/**/*.js', ['js']);
+	// gulp.watch('2016_spring/**/images/*', ['img']);
 });
